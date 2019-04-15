40e52dddc3e8b98073199fb5a2141a546299a2e5
Pandele George <pandelegeorge@telacad.ro>
2019-04-15 21:14:07 +0300

    add models to django

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
+# Register your models here.
 from django.contrib import admin
 
 # Register your models here.
+from .models import Person
+
+admin.site.register(Person)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+# Create your models here.
+class Person(models.Model):
+    id = models.AutoField(primary_key=True)
+    nume = models.CharField(max_length=50)
+    phonenumber = models.IntegerField()
+    mail = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = "personmodel"
+
+    def __str__(self):
+        return self.nume
