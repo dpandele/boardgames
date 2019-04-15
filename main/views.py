eb0e45616e4124e3d6a2a58f55ffd11255f45e96
Pandele George <pandelegeorge@telacad.ro>
2019-04-15 21:57:16 +0300

    save to database

--- main/views.py
+++ main/views.py
@@ -11,3 +11,23 @@ def formular(request):
 def formular_submit(request):
     return render(request, "main/raspunsformular.html",{'name':request.POST['nume'],'prenume':request.POST['prenume'],
                                                         'trimite':request.POST['trimite']})
+
+from .forms import PersonForm
+from .models import Person
+def person(request):
+    if request.method == 'POST':
+        form = PersonForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            person = Person()
+            person.nume = cd['nume']
+            person.mail = cd['email']
+            person.phonenumber = cd['phonenumber']
+            person.save()
+            form = PersonForm()
+        else:
+            print("Form is not valid")
+    else:
+        form = PersonForm()
+
+    return render(request, 'main/contact_form.html', {'form': form})
