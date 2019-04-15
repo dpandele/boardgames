a9a5dda0d30c08ef8b216c32d1d87ae1b4d31e18
Pandele George <pandelegeorge@telacad.ro>
2019-04-15 21:30:56 +0300

    add custom user form to django

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('formular/', views.formular, name="formular"),
+    path('formular_submit/', views.formular_submit, name="formular_submit"),
 ]
